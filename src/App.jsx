adf3394aa66c71654c0305cdb877c5f8a7d5350f
jdev92 <juliencavaillon6@gmail.com>
2023-08-25 16:06:48 +0200

    ajout Signup/Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Home from './pages/Home';
 import Offer from './pages/Offer';
+import Signup from "./pages/Signup";
+import Login from './pages/Login'
 import './App.css'
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home data={data} setData={setData} />} />
           <Route path="/offers/:id" element={<Offer data={data} setData={setData} />} />
+          <Route path="/signup" element={<Signup data={data} setData={setData} />} />
+          <Route path="/login" element={<Login data={data} setData={setData} />} />
         </Routes>
       </Router>
     </>
