ac5961a6298b55f557a073ffed627658f3cc8a5d
jdev92 <juliencavaillon6@gmail.com>
2023-08-28 12:30:08 +0200

    Implenment search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { useState } from "react"
-import Home from "./pages/Home"
-import Offer from "./pages/Offer"
-import Signup from "./pages/Signup"
-import Login from "./pages/Login"
-import Header from "./components/Header"
-import Cookies from "js-cookie"
-import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Home from "./pages/Home";
+import Offer from "./pages/Offer";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Header from "./components/Header";
+import Cookies from "js-cookie";
+import "./App.css";
 
 function App() {
-  const [data, setData] = useState([])
-  const [userToken, setUserToken] = useState(Cookies.get("userToken") || null)
+  const [data, setData] = useState([]);
+  const [userToken, setUserToken] = useState(Cookies.get("userToken") || null);
+  const [search, setSearch] = useState("");
 
+  // Stocker le token dans le state et les cookies
   const handleToken = token => {
     if (token) {
-      Cookies.set("userToken", token, { expires: 7 })
-      setUserToken(token)
+      Cookies.set("userToken", token, { expires: 7 });
+      setUserToken(token);
     } else {
-      Cookies.remove("userToken")
-      setUserToken(null)
+      // Supprime le token dans le state et les cookies
+      Cookies.remove("userToken");
+      setUserToken(null);
     }
-  }
+  };
 
   return (
     <>
       <Router>
-        <Header handleToken={handleToken} userToken={userToken} />
+        <Header
+          handleToken={handleToken}
+          userToken={userToken}
+          search={search}
+          setSearch={setSearch}
+        />
         <Routes>
-          <Route path="/" element={<Home data={data} setData={setData} />} />
+          <Route
+            path="/"
+            element={<Home data={data} setData={setData} search={search} />}
+          />
           <Route
             path="/offers/:id"
             element={<Offer data={data} setData={setData} />}
@@ -47,7 +58,7 @@ function App() {
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
